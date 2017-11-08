52a9075a3fc9bafdb3fd7f524b07f3f987625d3a
friendAAA <271817842@qq.com>
2017-11-08 11:24:43 +0800

    ·

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Header from '@/components/MyHeader'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path:'/header',
+      name:'header',
+      component:Header
     }
   ]
 })
